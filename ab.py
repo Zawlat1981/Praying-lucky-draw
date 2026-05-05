8c1d33595c813f0881b7ad42f57dd9628d883dcc
Aung Zawlat <103322801+Zawlat1981@users.noreply.github.com>
2026-05-05 16:49:03 +0700

    Refactor code structure and improve UI elements

--- ab.py
+++ ab.py
@@ -100,5 +100,7 @@ if st.button("ဘယ်သူတွေ တာဝန်ကျမလဲ ကြည�
 
 # ရလဒ်များကို ပြသခြင်း
 if 'leader' in st.session_state and 'teacher' in st.session_state:
-    st.markdown(f"### 🎤 ခွန်အားကြီးသော ဆုတောင်းခြင်း ဦးဆောင်သူ: **{st.session_state['leader']}**")
-    st.markdown(f"### 📖  ရှင်သန်လွတ်မြောက်ခြင်းသို့ ကျမ်းစာအားဖြင့် လမ်းပြသွန်သင်သူ: **{st.session_state['teacher']}**")
+    # ဦးဆောင်သူအတွက်
+    st.markdown(f"<h3 style='text-align: center; font-size: 20px;'>🙏 ခွန်အားကြီးသော ဆုတောင်းခြင်း ဦးဆောင်သူ - {st.session_state['leader']}</h3>", unsafe_allow_html=True)
+    # သွန်သင်သူအတွက် (variable ကို 'teacher' လို့ ပြောင်းလိုက်ပါတယ်)
+    st.markdown(f"<h3 style='text-align: center; font-size: 20px;'>📖 ရှင်သန်လွတ်မြောက်ခြင်းသို့ ကျမ်းစာအားဖြင့် လမ်းပြသွန်သင်သူ - {st.session_state['teacher']}</h3>", unsafe_allow_html=True)
